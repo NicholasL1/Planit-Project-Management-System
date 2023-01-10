2957283affa59201086080bcb59b73b137d5a3cc
Nicholas Lachhman <nickla@my.yorku.ca>
2023-01-10 12:53:50 -0500

    Added error handling middleware and async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,26 @@
-const registerUser = (req, res) => {
+// @desc Register a new user
+// @route /api/users
+// @access Public
+const registerUser = asycnHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  //Register user Validation
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please include all required fields!')
+  }
+
   res.send('Register Route')
-}
+})
 
-const loginUser = (req, res) => {
+// @desc Login a user
+// @route /api/users/login
+// @access Public
+const loginUser = asycnHandler(async (req, res) => {
   res.send('Login Route')
-}
+})
 
-module.export = {
+module.exports = {
   registerUser,
   loginUser
 }
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { erroHandler, errorHandler } = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 6000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
-  res.status(200).json({ mesage: 'Welcome to the Managament System API' })
+  res.status(200).json({ message: 'Welcome to the Managament System API' })
 })
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
