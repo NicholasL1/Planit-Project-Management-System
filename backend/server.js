8e00cfbc62763b7eb4806080a78a112e58469850
Nicholas Lachhman <nickla@my.yorku.ca>
2023-01-10 12:26:30 -0500

    Set up backend with express, added register and login routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 6000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).json({ mesage: 'Welcome to the Managament System API' })
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
