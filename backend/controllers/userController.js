e586af004b7ff72ed54eca3289c4808fe9a8d000
Nicholas Lachhman <nickla@my.yorku.ca>
2023-01-10 13:23:58 -0500

    Added user backend validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,7 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
   })
 
   if (user) {
-    res.status(200).json({
+    res.status(201).json({
       _id: user.id,
       name: user.name,
       email: user.email
@@ -50,7 +50,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route')
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  // Checking if user and passwords match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid credentials!')
+  }
 })
 
 module.exports = {
